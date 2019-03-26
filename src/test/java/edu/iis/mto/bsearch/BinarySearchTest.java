02c322e721b7dd09a0c737d1f2ffec7ec904b4df
Igor Jarek <igij@o2.pl>
2019-03-26 16:48:01 +0100

    testy_v1 first test

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,31 +8,31 @@ public class BinarySearchTest {
 
     @Test
     public void sequenceHasOneLengthAndContainsItem() {
+        int key = 10;
+        int[] array = {key};
+        SearchResult result = BinarySearch.search(key, array);
+
+        assertEquals(1, array.length);
+        assertTrue(result.isFound());
+    }
+
+    @Test public void sequenceHasOneLengthAndNotContainsItem() {
 
     }
 
-    @Test
-    public void sequenceHasOneLengthAndNotContainsItem() {
+    @Test public void sequenceHasMoreThanOneLengthAndContainsItemAtFirstPos() {
 
     }
 
-    @Test
-    public void sequenceHasMoreThanOneLengthAndContainsItemAtFirstPos() {
+    @Test public void sequenceHasMoreThanOneLengthAndContainsItemAtLastPos() {
 
     }
 
-    @Test
-    public void sequenceHasMoreThanOneLengthAndContainsItemAtLastPos() {
+    @Test public void sequenceHasMoreThanOneLengthAndContainsItemAtMiddlePos() {
 
     }
 
-    @Test
-    public void sequenceHasMoreThanOneLengthAndContainsItemAtMiddlePos() {
-
-    }
-
-    @Test
-    public void sequenceHasMoreThanOneLengthAndNotContainsItem() {
+    @Test public void sequenceHasMoreThanOneLengthAndNotContainsItem() {
 
     }
 }
