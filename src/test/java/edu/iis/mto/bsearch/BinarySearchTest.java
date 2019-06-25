42cbcd2226c167d3c34002ae3d46a4dee7581bba
Igor <igij@o2.pl>
2019-06-25 17:39:09 +0200

    edit comment about variable overflow

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,15 +9,19 @@ public class BinarySearchTest {
 
     /*
         test_v3
-        implementacja przechowuje lewy oraz prawy indeks w zmiennych typu int o dodatnik zakresie 0x7FFFFFFF każdy
+        implementacja przechowuje lewy oraz prawy indeks w zmiennych typu int o dodatnim zakresie 0x7FFFFFFF każdy
         jeżeli tabelica wejściowa będzia miała maksymalną długość 0x7FFFFFFF, to w instrukcji dla "prawej połówki"
         center = (start + end) / 2; nastąpi przekręcenie licznika a w konsekwencji błędne działanie programu.
+
+        EDIT :
+        Kompilator rzuca wyjątkiem OutOfMemoryError.
+        Tablica int'ów o rozmiarze 0x7FFFFFFF wymaga 8GB RAM'u, więc ten błąd działa tylko teoretycznie.
      */
 
     @Test
     public void sequenceReverse() {
-        int key = 10;
-        int[] array = {key, key - 1, key - 2, key - 3};
+        int key = 1000000000;
+        int[] array = {Integer.MAX_VALUE, key, 0, -1000000000, Integer.MIN_VALUE};
         SearchResult result = BinarySearch.search(key, array);
         assertThat(result.isFound(), is(equalTo(false)));
     }
@@ -25,14 +29,14 @@ public class BinarySearchTest {
     @Test(expected = IllegalArgumentException.class)
     public void sequenceNull() {
         int key = 10;
-        SearchResult result = BinarySearch.search(key, null);
+        BinarySearch.search(key, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void sequenceHasZeroLength() {
         int key = 10;
         int[] array = {};
-        SearchResult result = BinarySearch.search(key, array);
+        BinarySearch.search(key, array);
     }
 
     @Test
@@ -58,8 +62,8 @@ public class BinarySearchTest {
 
     @Test
     public void sequenceHasMoreThanOneLengthAndContainsItemAtFirstPos() {
-        int searchKey = 10;
-        int[] array = {searchKey, searchKey + 1, searchKey + 2, searchKey + 3};
+        int searchKey = Integer.MIN_VALUE;
+        int[] array = {searchKey, -1000000000, 0, 1000000000, Integer.MAX_VALUE};
         SearchResult result = BinarySearch.search(searchKey, array);
 
         assertThat(array.length > 1, is(equalTo(true)));
@@ -69,9 +73,9 @@ public class BinarySearchTest {
 
     @Test
     public void sequenceHasMoreThanOneLengthAndContainsItemAtLastPos() {
-        int searchKey = 10;
-        int[] array = {searchKey, searchKey + 1, searchKey + 2, searchKey + 3};
-        SearchResult result = BinarySearch.search(searchKey + 3, array);
+        int searchKey = Integer.MAX_VALUE;
+        int[] array = {Integer.MIN_VALUE, -1000000000, 0, 1000000000, searchKey};
+        SearchResult result = BinarySearch.search(searchKey, array);
 
         assertThat(array.length > 1, is(equalTo(true)));
         assertThat(result.isFound(), is(equalTo(true)));
@@ -80,9 +84,9 @@ public class BinarySearchTest {
 
     @Test
     public void sequenceHasMoreThanOneLengthAndContainsItemAtMiddlePos() {
-        int searchKey = 10;
-        int[] array = {searchKey, searchKey + 1, searchKey + 2, searchKey + 3, searchKey + 4};
-        SearchResult result = BinarySearch.search(searchKey + 2, array);
+        int searchKey = 0;
+        int[] array = {Integer.MIN_VALUE, -1000000000, searchKey, 1000000000, Integer.MAX_VALUE};
+        SearchResult result = BinarySearch.search(searchKey, array);
 
         assertThat(array.length > 1, is(equalTo(true)));
         assertThat(result.isFound(), is(equalTo(true)));
@@ -91,9 +95,9 @@ public class BinarySearchTest {
 
     @Test
     public void sequenceHasMoreThanOneLengthAndNotContainsItem() {
-        int searchKey = 10;
-        int[] array = {searchKey, searchKey + 1, searchKey + 2, searchKey + 3};
-        SearchResult result = BinarySearch.search(searchKey + 4, array);
+        int searchKey = 12345678;
+        int[] array = {Integer.MIN_VALUE, -1000000000, 0, 1000000000, Integer.MAX_VALUE};
+        SearchResult result = BinarySearch.search(searchKey, array);
 
         assertThat(array.length > 1, is(equalTo(true)));
         assertThat(result.isFound(), is(equalTo(false)));
